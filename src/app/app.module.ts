80e1feee9171f80aa706a924357c5bf85fc5fd18
Kshitij Goel <goelkshitij96@gmail.com>
2018-11-17 18:03:19 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PubNubAngular } from 'pubnub-angular2';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PubNubAngular],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
