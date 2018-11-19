f7129f0948ac629eee2ad7bfb43a1fb640179799
Kshitij Goel <goelkshitij96@gmail.com>
2018-11-18 19:03:14 -0500

    SideBar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
