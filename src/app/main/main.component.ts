81e2bb2c16926fa636590d64e6954d5119fe431e
Kshitij Goel <goelkshitij96@gmail.com>
2018-11-27 13:38:58 -0500

    Testing phase

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {PubNubAngular} from 'pubnub-angular2';
+
+class TransmitObject {
+  message: string;
+  deviceType: string;
+
+  getMessage() {
+    return this.message;
+  }
+
+  setMessage(msg: string) {
+    this.message = msg;
+  }
+
+  getDeviceType() {
+    return this.deviceType;
+  }
+
+  setDeviceType(str: string) {
+    this.deviceType = str;
+  }
+}
 
 @Component({
   selector: 'app-main',
@@ -7,9 +29,253 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  pubKey = 'pub-c-a48eea9b-bec6-437f-a198-c629b1d05c4c';
+  subKey = 'sub-c-b3f1894c-b61b-11e8-9c8c-5aa277adf39c';
+  subscribeChannel = 'Hub Channel';
+  publishChannel = 'Mobile Channel';
+  pubnub_global;
 
-  ngOnInit() {
+  overrideUnique = '00:00:00:00:00:00';
+  transmitObject = new TransmitObject;
+
+  overrideDis = false;
+  redDis = false;
+  yellowDis = false;
+  greenDis = false;
+  overrideStatus = false;
+  redStatus = false;
+  yellowStatus = false;
+  greenStatus = false;
+
+  sensorDistance: number;
+
+  constructor(pubnub: PubNubAngular) {
+    pubnub.init({publishKey: this.pubKey, subscribeKey: this.subKey});
+    pubnub.grant({
+        channels: [this.publishChannel, this.subscribeChannel],
+        authKeys: [this.pubKey, this.subKey],
+        ttl: 5,
+        read: true,
+        write: true
+      },
+      (status) => {
+        console.log(status);
+      });
+    this.pubnub_global = pubnub;
   }
 
+  ngOnInit() {
+    this.redDis = true;
+    this.yellowDis = true;
+    this.greenDis = true;
+    this.transmitObject.deviceType = 'webapp';
+    // this.transmitObject.setDeviceType('webapp');
+    this.pubnubSubscribe();
+  }
+
+  pubnubSubscribe() {
+    const self = this;
+    this.pubnub_global.addListener({
+      presence: function(m) {
+        console.log(m);
+      },
+      message: function(message) {
+        const extract = message.message;
+        console.log('Received in subscribe:');
+        console.log(extract);
+        self.ServerTask(extract.map);
+      }
+    });
+    this.pubnub_global.subscribe({
+      channels: [ this.subscribeChannel ],
+      withPresence: true
+    });
+  }
+
+  pubnubPublish(transmitObject: TransmitObject) {
+    this.pubnub_global.publish(
+      {
+        message: JSON.stringify(transmitObject),
+        channel: this.publishChannel
+      },
+      function (status, response) {
+        if (status.error) {
+          console.log(status);
+        } else {
+          console.log('message Published w/ timetoken', response.timetoken);
+        }
+      }
+    );
+  }
+
+  ServerTask(trans: any) {
+    const recs = trans.message.split('#');
+    if (trans.deviceType.localeCompare('android') === 0 || trans.deviceType.localeCompare('webapp') === 0) {
+      if (recs[3].localeCompare('1') === 0 && recs[1].localeCompare(this.overrideUnique) !== 0) {
+        this.setDis('dis', 'dis', 'dis', 'dis');
+      } else if (recs[3].localeCompare('0') === 0 && recs[1].localeCompare(this.overrideUnique) !== 0) {
+        this.setDis('en', 'dis', 'dis', 'dis');
+      }
+    } else if (trans.deviceType.localeCompare('hub') === 0) {
+      this.setStatus(recs[3], recs[5], recs[7]);
+      this.sensorDistance = recs[9];
+    }
+  }
+
+  ClientTask(transmitObject: TransmitObject) {
+    this.pubnubPublish(transmitObject);
+  }
+
+  overrideClick() {
+    this.overrideStatus = !this.overrideStatus;
+    this.redDis = !this.redDis;
+    this.yellowDis = !this.yellowDis;
+    this.greenDis = !this.greenDis;
+    if (this.overrideStatus === true) {
+      const msg = 'webapp#' + this.overrideUnique + '#override#' + this.getStatus('override') + '#red#' + this.getStatus('red');
+      const msg2 = msg + '#yellow#' + this.getStatus('yellow') + '#green#' + this.getStatus('green');
+      this.transmitObject.message = msg2;
+      this.ClientTask(this.transmitObject);
+    } else {
+      if (this.redStatus) {
+        document.getElementById('redSlider').click();
+      }
+      if (this.yellowStatus) {
+        document.getElementById('yellowSlider').click();
+      }
+      if (this.greenStatus) {
+        document.getElementById('greenSlider').click();
+      }
+      this.redStatus = false;
+      this.yellowStatus = false;
+      this.greenStatus = false;
+      const msg = 'webapp#' + this.overrideUnique + '#override#' + this.getStatus('override') + '#red#' + this.getStatus('red');
+      const msg2 = msg + '#yellow#' + this.getStatus('yellow') + '#green#' + this.getStatus('green');
+      this.transmitObject.message = msg2;
+      this.ClientTask(this.transmitObject);
+    }
+  }
+
+  redClick() {
+    this.redStatus = !this.redStatus;
+    console.log(this.redStatus);
+    if (this.redStatus === true) {
+      const msg = 'webapp#' + this.overrideUnique + '#override#' + this.getStatus('override') + '#red#' + this.getStatus('red');
+      const msg2 = msg + '#yellow#' + this.getStatus('yellow') + '#green#' + this.getStatus('green');
+      this.transmitObject.message = msg2;
+      // this.transmitObject.setMessage(msg2);
+      this.ClientTask(this.transmitObject);
+    } else {
+      const msg = 'webapp#' + this.overrideUnique + '#override#' + this.getStatus('override') + '#red#' + this.getStatus('red');
+      const msg2 = msg + '#yellow#' + this.getStatus('yellow') + '#green#' + this.getStatus('green');
+      this.transmitObject.message = msg2;
+      // this.transmitObject.setMessage(msg2);
+      this.ClientTask(this.transmitObject);
+    }
+  }
+
+  yellowClick() {
+    this.yellowStatus = !this.yellowStatus;
+    console.log(this.yellowStatus);
+    if (this.yellowStatus === true) {
+      const msg = 'webapp#' + this.overrideUnique + '#override#' + this.getStatus('override') + '#red#' + this.getStatus('red');
+      const msg2 = msg + '#yellow#' + this.getStatus('yellow') + '#green#' + this.getStatus('green');
+      this.transmitObject.message = msg2;
+      // this.transmitObject.setMessage(msg2);
+      this.ClientTask(this.transmitObject);
+    } else {
+      const msg = 'webapp#' + this.overrideUnique + '#override#' + this.getStatus('override') + '#red#' + this.getStatus('red');
+      const msg2 = msg + '#yellow#' + this.getStatus('yellow') + '#green#' + this.getStatus('green');
+      this.transmitObject.message = msg2;
+      // this.transmitObject.setMessage(msg2);
+      this.ClientTask(this.transmitObject);
+    }
+  }
+
+  greenClick() {
+    this.greenStatus = !this.greenStatus;
+    console.log(this.greenStatus);
+    if (this.greenStatus === true) {
+      const msg = 'webapp#' + this.overrideUnique + '#override#' + this.getStatus('override') + '#red#' + this.getStatus('red');
+      const msg2 = msg + '#yellow#' + this.getStatus('yellow') + '#green#' + this.getStatus('green');
+      this.transmitObject.message = msg2;
+      // this.transmitObject.setMessage(msg2);
+      this.ClientTask(this.transmitObject);
+    } else {
+      const msg = 'webapp#' + this.overrideUnique + '#override#' + this.getStatus('override') + '#red#' + this.getStatus('red');
+      const msg2 = msg + '#yellow#' + this.getStatus('yellow') + '#green#' + this.getStatus('green');
+      this.transmitObject.message = msg2;
+      // this.transmitObject.setMessage(msg2);
+      this.ClientTask(this.transmitObject);
+    }
+  }
+
+  getStatus(str: string) {
+    if (str.localeCompare('override') === 0) {
+      if (this.overrideStatus) {
+        return '1';
+      } else {
+        return '0';
+      }
+    } else if (str.localeCompare('red') === 0) {
+      if (this.redStatus) {
+        return '1';
+      } else {
+        return '0';
+      }
+    } else if (str.localeCompare('yellow') === 0) {
+      if (this.yellowStatus) {
+        return '1';
+      } else {
+        return '0';
+      }
+    } else if (str.localeCompare('green') === 0) {
+      if (this.greenStatus) {
+        return '1';
+      } else {
+        return '0';
+      }
+    }
+  }
+
+  setStatus(red: string, yellow: string, green: string) {
+    if (red.localeCompare('0') === 0) {
+      this.redStatus = false;
+    } else if (red.localeCompare('1') === 0) {
+      this.redStatus = true;
+    }
+    if (yellow.localeCompare('0') === 0) {
+      this.yellowStatus = false;
+    } else if (yellow.localeCompare('1') === 0) {
+      this.yellowStatus = true;
+    }
+    if (green.localeCompare('0') === 0) {
+      this.greenStatus = false;
+    } else if (green.localeCompare('1') === 0) {
+      this.greenStatus = true;
+    }
+  }
+
+  setDis(over: string, red: string, yellow: string, green: string) {
+    if (over.localeCompare('dis') === 0) {
+      this.overrideDis = true;
+    } else if (over.localeCompare('en') === 0) {
+      this.overrideDis = false;
+    }
+    if (red.localeCompare('dis') === 0) {
+      this.redDis = true;
+    } else if (red.localeCompare('en') === 0) {
+      this.redDis = false;
+    }
+    if (yellow.localeCompare('dis') === 0) {
+      this.yellowDis = true;
+    } else if (yellow.localeCompare('en') === 0) {
+      this.yellowDis = false;
+    }
+    if (green.localeCompare('dis') === 0) {
+      this.greenDis = true;
+    } else if (green.localeCompare('en') === 0) {
+      this.greenDis = false;
+    }
+  }
 }
